6f8ec349544172bfa74c4ca96450c823cf2506f3
sahulhameed.iqbal <sahulhameed.iqbal@rencata.com>
2023-02-10 12:43:25 +0530

    UniversityStaff

---
+++ UniversityPortal/Controllers/UniversityStaffController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UniversityPortal.Common;
+using UniversityPortal.Interfaces.Services;
+using UniversityPortal.Models;
+using UniversityPortal.Services;
+
+namespace UniversityPortal.Controllers
+{
+    public class UniversityStaffController : Controller
+    {
+        private readonly IUniversityStaffService _universityStaffService;
+
+        public UniversityStaffController(IUniversityStaffService universityStaffService)
+        {
+            _universityStaffService = universityStaffService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AddNew()
+        {            
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddNew(UniversityStaffViewModel model, bool isAdd)
+        {
+            await _universityStaffService.Create(model, isAdd);
+            return View();
+        }
+    }
+}
